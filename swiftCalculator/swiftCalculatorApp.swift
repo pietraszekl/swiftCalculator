48c6362abb3738ee2f18af2842f92f670884d653
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2022-10-05 19:55:04 +0200

    Initial Commit

---
+++ swiftCalculator/swiftCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftCalculatorApp.swift
+//  swiftCalculator
+//
+//  Created by Lukasz Pietraszek on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
